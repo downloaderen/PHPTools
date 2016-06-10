b2a0897cfb27843a1551e8e6f16f91130a991390
Güner Yildirim <liltoto@gmail.com>
2016-06-10 10:52:39 +0200

    Version 1.1

--- JSONxmlRss/index.php
+++ JSONxmlRss/index.php
@@ -5,24 +5,28 @@
 
     $db = new DB('localhost','cmk_php_nyhedssite');
 
-    class UltraDynamic{
+    class API{
         public function createJSON($arr){
             $name = new stdClass;
             foreach($arr as $key => $value){
                 $name->$key = $value;
             }
+            header('Content-Type: application/json');
             return json_encode($name, JSON_FORCE_OBJECT);
             //return $name;
         }
-        private function array_to_RSS(array $arr, SimpleXMLElement $xml)
-        {
+        private function array_to_XML(array $arr, SimpleXMLElement $xml){
             foreach ($arr as $key => $value) {
                 if(is_array($value)){
                     if(is_int($key)){
-                        //echo '<pre>' , print_r($value), '</pre>';
-                        $this->array_to_RSS($value['item'], $xml->addChild('item'));
+                        if(!is_array(current($value))){
+                            // echo '<pre>' , print_r($value), '</pre>';
+                            $this->array_to_XML($value, $xml->addChild('item'));
+                        } else {
+                            $this->array_to_XML($value[array_keys($value)[0]], $xml->addChild(array_keys($value)[0]));
+                        }
                     } else {
-                        $this->array_to_RSS($value, $xml->addChild($key));
+                        $this->array_to_XML($value, $xml->addChild($key));
                     }
                 } else {
                     $xml->addChild($key, $value);
@@ -30,17 +34,18 @@
             }
             return $xml;
         }
-        public function createRSS($arr){
-            return $this->array_to_RSS($arr, new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><rss/>'))->asXML();
+        public function createXML($arr, $root = '<root/>'){
+            header('Content-Type: application/xml');
+            return $this->array_to_XML($arr, new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?>'.$root))->asXML();
         }
     }
     $category_id = 1;
     $category = $db->Single('SELECT * FROM categories WHERE category_id = :id', array(':id' => $category_id));
 
-    $ultra = new UltraDynamic();
+    $ultra = new API();
     $arr = array(
         'channel' => array(
-            'id' => $category->category_id, 
+            'id' => $category->category_id,
             'title' => $category->category_title,
             'description' => $category->category_description
         )
@@ -50,23 +55,16 @@
                 FROM news
                 INNER JOIN categories ON category_id = news.fk_categories_id
                 WHERE category_id = :id ORDER BY news_postdate DESC', array(':id' => $category_id));
-    
+
     foreach($allNews as $value)
-    {	
-        $arr['channel'][]['item'] = array(
+    {
+        $arr['channel'][] = array(
             'title' => $value->news_title,
             'description' => htmlentities($value->news_content)
         );
     }
 
-    //echo '<pre>', print_r($ultra->createJSON($arr));
-
-    // header('Content-Type: application/xml');
-    // echo $ultra->createRSS($arr);
-    // $ultra->createXML($arr)->asXML('feeds/name.xml');
-    //print $ultra->createXML($arr)->asXML();
-    header('Content-Type: application/json');
-    echo $ultra->createJSON($arr);
+    echo $ultra->createXML($arr, '<rss/>');
 
 
 ?>
